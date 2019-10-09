7a4eef94868c84719486bcaedd1516d510cc51c2
David Svensson <davidsvson@gmail.com>
2019-10-09 10:00:42 +0200

    points label in inspector

---
+++ Assets/Editor/SquareScriptEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ClickSquareController))]
+public class SquareScriptEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ClickSquareController clickSquare = (ClickSquareController) target;
+
+        EditorGUILayout.LabelField("Points", clickSquare.points.ToString());
+
+    }
+
+
+}
---
+++ Assets/Scripts/ClickSquareController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class ClickSquareController : MonoBehaviour
+{
+
+    [HideInInspector]
+    public int points = 0;
+
+    public List<Color> colors;
+
+
+    SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        SetRandomColor();
+    }
+
+
+    private void OnMouseDown()
+    {
+        points++;
+        SetRandomColor();
+    }
+
+
+    void SetRandomColor()
+    {
+        Color newColor = spriteRenderer.color;
+        while (newColor == spriteRenderer.color)
+        {
+            newColor = colors[Random.Range(0, colors.Count)];
+        }
+
+        spriteRenderer.color = newColor;
+    }
+
+
+}
